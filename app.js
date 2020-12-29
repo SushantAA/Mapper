3e543cf269175a325a7d7261b778b189c5c261d5
SushantAA <sushant.co19@gmail.com>
2020-12-29 06:19:08 +0530

    joi add]

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');;
-const ejsMate = require('ejs-mate')
+const ejsMate = require('ejs-mate');
+const { type } = require('os');
+const catchAsync = require('./utilities/catchAsync');
+const expresError = require('./utilities/expressError');
+const Joi = require('joi');
+const { required } = require('joi');
+const { validate } = require('./models/campground');
 
 const app = express();
 
@@ -20,58 +26,88 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 
+const validatecg = (req,res,next) => {
+    const campgroundSchema = Joi.object({
+        // Campground : Joi.object({
+            title : Joi.string().required(),
+            price : Joi.number().required().min(0),
+            image :  Joi.string().required(),
+            location : Joi.string().required(),
+            discription : Joi.string().required()
+        // }).required()
+    });
+
+    const {error} = campgroundSchema.validate(req.body);
+
+    if(error){
+        const msg = error.details.map(el=>el.message).join(',');
+        console.log(msg);
+        throw new expresError(msg,400);
+    }else{
+        next();
+    }
+
+}
+
 app.get('/', (req,res) => {
     res.render('home');
 });
 
-app.get('/cg/new', async (req,res) => {
+app.get('/cg/new', catchAsync( async (req,res) => {
     res.render('cg/new');
-});
+}));
 
-app.get('/cg',async (req,res) => {
+app.get('/cg', catchAsync( async (req,res) => {
     const all_cg = await Campground.find({});
     // res.send(all_cg);
     res.render('cg/index',{all_cg})
-});
+}));
 
-app.post('/cg', async (req,res) => {
+app.post('/cg', validatecg ,catchAsync( async (req,res) => {
     const a = await Campground(req.body);
     await a.save();
-    // res.send(req.body);
     res.redirect('/cg');
-});
+}));
 
-app.put('/cg/:id', async(req,res) => {
+app.put('/cg/:id', catchAsync( async(req,res,next) => {
     const {id} = req.params;
     const a = await Campground.findByIdAndUpdate(id,req.body);
-    // res.send('updated =}');
     res.redirect(`/cg/${id}`);
-});
+}));
 
-app.get('/cg/:id/edit', async (req,res) => {
+app.get('/cg/:id/edit', catchAsync( async (req,res) => {
     const {id} = req.params;
-    res.render('cg/edit',{id});
-});
+    const a = await Campground.findById(id);
+    res.render('cg/edit',{a});
+}));
 
-app.delete('/cg/:id', async (req,res) => {
+app.delete('/cg/:id', catchAsync( async (req,res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
-    // res.send('DELETED @#');
     res.redirect(`/cg`);
-});
+}));
 
-app.get('/cg/:id', async (req,res) => {
+app.get('/cg/:id', catchAsync( async (req,res) => {
     const {id}  = req.params;
     const a = await Campground.findById(id);
     res.render('cg/show_detail',{a});
-});
+}));
 
 
 
-app.get('/cg',async (req,res)=> {
+app.get('/cg', catchAsync( async (req,res)=> {
     const camp = new Campground({title: "My backyard" , price : 100});
     await camp.save();
     res.send(camp);
+}));
+
+app.all('*',(req,res,next)=>{
+    next(new expresError('page went wrong',404));
+});
+
+app.use((err,req,res,next)=>{
+    const {statusCode = 500 , message = 'some thing went wrong'} = err;
+    res.status(statusCode).render('error.ejs',{err});
 });
 
 app.listen(3030, () => {
