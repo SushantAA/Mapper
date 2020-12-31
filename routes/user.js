091a5550bcc57bf8d4302d74cfd260cfdc895085
SushantAA <sushant.co19@gmail.com>
2020-12-31 10:09:40 +0530

    feat : register comp

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router =  express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utilities/catchAsync');
 
 router.get('/register',(req,res)=>{
     res.render('users/register');
 });
 
-router.post('/register', async ( req, res) => {
-    res.send(req.body);
-});
+router.post('/register', catchAsync (async ( req, res) => {
+    try{
+        console.log(req.body);
+        const {email , username , password} = req.body;
+        const user = new User({email,username});
+        const registeredUser = await User.register(user,password);
+        console.log(registeredUser);
+        req.flash('success','welcome to mapper');
+        res.redirect('/cg');
+    }catch(e){
+        req.flash('error',e.message);
+        res.redirect('register');
+    }
+}));
 
 module.exports = router;
