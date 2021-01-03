fbe2ee2888231fb9822dce66b0030d1896b610ea
SushantAA <sushant.co19@gmail.com>
2021-01-03 19:33:46 +0530

    feat : show map complete

--- public/scripts/showMap.js
+++ public/scripts/showMap.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: ag, // starting position [lng, lat]
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: ag.geometry.coordinates, // starting position [lng, lat]
     zoom: 11 // starting zoom
 });
 
 new mapboxgl.Marker()
-    .setLngLat(ag)
+    .setLngLat(ag.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset:25 })
+            .setHTML(
+                `<h3>${ag.title}</h3><p>${ag.location}</p>`
+            )
+    )
     .addTo(map);
